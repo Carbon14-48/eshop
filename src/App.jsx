1e12f9a8163a0cac0f948fa035b47a0a220720dd
Carbon14-48 <mohammedberrichi4@gmail.com>
2025-09-02 13:39:07 +0100

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import Hero from "./components/Hero";
 import Category from "./components/Category";
 import Category2 from "./components/Category2";
 import Services from "./components/Services";
+import Banner from "./components/Banner";
+import headphone from "./assets/hero/headphone.png";
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smike",
+  date: "19 Jan to 29 Jan",
+  image: headphone,
+  title2: "Air Solo Base",
+  title3: "Winter Sale",
+  title4:
+    "Lorem ipusm , dolor , sit amat conscutar  Tottnehma best Teams In england and Harry Kane best player in the world",
+  bgColor: "#f42c37",
+};
 function App() {
   return (
     <div className="bg-gray-50 dark:bg-blue-950 dark:text-white overflow-hidden duration-200">
@@ -12,6 +25,7 @@ function App() {
       <Category />
       <Category2 />
       <Services />
+      <Banner data={BannerData} />
     </div>
   );
 }
